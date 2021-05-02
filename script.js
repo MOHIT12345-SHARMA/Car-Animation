8874be0251a49ebfa47bd9cc44fe7d3bc7655387
MOHIT12345-SHARMA <mohitvashistha627@gmail.com>
2021-05-02 20:30:49 +0530

    Car-Animation

---
+++ script.js
@@ -0,0 +1,16 @@
+window.onload = function(){
+document.getElementById("myAudio").play();
+}
+// myAudio = new Audio('formula_1.mp3'); 
+// if (typeof myAudio.loop == 'boolean')
+// {
+//     myAudio.loop = true;
+// }
+// else
+// {
+//     myAudio.addEventListener('ended', function() {
+//         this.currentTime = 0;
+//         this.play();
+//     }, false);
+// }
+// myAudio.play();
